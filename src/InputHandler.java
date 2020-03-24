9a94b423e4054d6df96db99108f5959f794c7fe1
Nicholas Mims <nicholaslmims@gmail.com>
2020-03-24 01:09:48 -0400

    Added SaveCommand and the InputHandler

---
+++ src/InputHandler.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class InputHandler {
+	
+	private HashMap<String, Command> commands;
+	
+	public InputHandler(Document doc) {
+		
+		commands = new HashMap<String, Command>();
+		commands.put("load", new LoadCommand(doc));
+		commands.put("save", new SaveCommand(doc));
+		commands.put("spell", new SpellCommand(doc));
+		//commands.put("print", new PrintCommand(doc));
+	}
+	
+	public void inputEntered(String data) {
+		if(commands.containsKey(data)) {
+			commands.get(data).execute();
+		} else {
+			System.out.println("Invalid Command!\n");
+		}
+	}
+}
