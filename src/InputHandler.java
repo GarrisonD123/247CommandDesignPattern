6fb304cf85f18b1b6e73c4ce774f60fbac402ee6
Nicholas Mims <nicholaslmims@gmail.com>
2020-03-24 01:20:42 -0400

    Added comments to InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,19 +1,39 @@
 import java.util.HashMap;
 
+/**
+ * Handles the input of commands for the document.
+ */
 public class InputHandler {
-	
+	/**
+	 * HashMap to hold the inputs to match to commands.
+	 * [String -> Command]:
+	 * "load" -> SpellCommand
+	 * "save" -> SaveCommand
+	 * "spell" -> SpellCommand
+	 * "print" -> PrintCommand
+	 */
 	private HashMap<String, Command> commands;
 	
+	/**
+	 * Constructs the InputHandler.
+	 * @param doc	The Document the commands should be executed onto.
+	 */
 	public InputHandler(Document doc) {
-		
 		commands = new HashMap<String, Command>();
+		
+		//Sets each String key to a Command value.
 		commands.put("load", new LoadCommand(doc));
 		commands.put("save", new SaveCommand(doc));
 		commands.put("spell", new SpellCommand(doc));
 		commands.put("print", new PrintCommand(doc));
 	}
 	
+	/**
+	 * Checks if the input entered is a key, and if so executes the corresponding command.
+	 * @param data	The command typed into the console.
+	 */
 	public void inputEntered(String data) {
+		//Checks if the entered input is a command.
 		if(commands.containsKey(data)) {
 			commands.get(data).execute();
 		} else {
